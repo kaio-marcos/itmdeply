c86eff251c1fb7bef5eaa189c543ad81c6dc5cd7
kaio marcos <kaiomarcos2015@outlook.com>
2019-10-02 15:20:26 -0300

    Atualização

--- debugador/settings.py
+++ debugador/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'meusistema'
+    'meusistema',
+    'bootstrapform'   
 ]
 
 MIDDLEWARE = [
--- meusistema/models.py
+++ meusistema/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Cadastro(models.Model):
+    nome = models.CharField(
+        max_length = 255,
+        verbose_name = 'Nome',
+    )
+
+    sobrenome = models.CharField(
+        max_length = 255,
+        verbose_name= 'Sobrenome',
+    )
+
+    email = models.EmailField(
+        max_length = 255,
+        verbose_name = 'E-mail', 
+        unique = True,
+    )
+    senha = models.CharField(
+        max_length = 10,
+        verbose_name = 'Senha',
+    )
+
+    def __str__(self):
+        return (f'{self.nome} + {self.sobrenome}')
